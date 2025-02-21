f26cc746145c7ebc5d53154214b1994c93a8de3c
Johan Clifford <johanclifford746@gmail.com>
2025-02-21 15:13:41 +0100

    Created Login,Register,Reset page and prepared userschema in file and on mongodb. Next is api calls and more setup

---
+++ src/app/(routes)/login/page.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Login() {
+
+    // const usernameRegex = /^[a-zA-Z0-9._%+-]{5,20}$/
+    // const passwordRegex = /^[a-zA-Z0-9._%+!-]{15,}$/
+
+  return (
+    <div className='flex flex-col items-center'>
+        <h1 className='mb-4 mt-4 text-xl sm:text-2xl md:text-3xl lg:text-4xl font-rock text-white text-stroke-title text-shadow-xl'>Login</h1>
+        <form className='flex flex-col items-center h-auto min-h-[30vh] w-full sm:w-[70vw] md:w-[60vw] lg:w-[50vw] xl:w-[40vw] bg-[#A5A5A5] bg-opacity-[75%] border border-black rounded-md p-8 gap-4 mb-2'>
+            <label htmlFor="loginName" className='sr-only'>Login Username</label>
+            <input
+                className='border border-black outline-none w-[95%] rounded-md bg-gray-400/20 placeholder:font-notojp placeholder:text-white text-white text-sm sm:text-base md:text-lg lg:text-xl p-2 sm:p-3 text-shadow-xl'
+                type="text"
+                id='loginName'
+                name='loginName'
+                required
+                placeholder='Enter Username'
+                minLength={5}
+                maxLength={20}
+                autoComplete='off'
+                spellCheck={false}
+                />
+            <label htmlFor="loginPassword" className='sr-only'>Login Password</label>
+            <input
+                className='border border-black outline-none w-[95%] rounded-md bg-gray-400/20 placeholder:font-notojp placeholder:text-white text-white text-sm sm:text-base md:text-lg lg:text-xl p-2 sm:p-3 text-shadow-xl'
+                type="password"
+                id='loginPassword'
+                name='loginPassword'
+                required
+                placeholder='Enter Password'
+                minLength={15}
+                autoComplete='off'
+                spellCheck={false}
+                />
+            <div className='flex flex-col items-center w-full gap-1'>
+                <Link href='/register'><p className='font-notojp text-white opacity-75 text-xs sm:text-sm text-shadow-xl'>Register</p></Link>
+                <Link href='/reset'><p className='font-notojp text-white opacity-75 text-xs sm:text-sm text-shadow-xl'>Forgot Password?</p></Link>
+            </div>
+        </form>
+        <div className='flex flex-row justify-end h-auto w-full'>
+            <button type='submit' className='p-2 w-[40%] sm:w-[30%] py-2 bg-[#E19B1A] border border-black rounded-md font-notojp text-white text-sm sm:text-base md:text-lg text-stroke text-shadow-xl transform transition-all duration-100 ease-in-out hover:scale-105 active:scale-95'>Login</button>
+        </div>
+    </div>
+  )
+}
+
---
+++ src/app/models/User.ts
@@ -0,0 +1,68 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { ObjectId } from "mongoose";
+
+interface IPost extends Document {
+    postId: number,
+    title: string,
+    content: string
+    createdAt: Date
+    updatedAt: Date
+}
+
+interface IComment extends Document {
+    commentId: number,
+    userId: ObjectId,
+    content: string,
+    createdAt: Date
+    updatedAt: Date
+}
+
+interface IUser extends Document {
+    _id: ObjectId,
+    email: string,
+    username: string,
+    password: string,
+    refreshToken: string,
+    gender: string,
+    weight: number,
+    age: number,
+    style: string,
+    followers: number,
+    following: number,
+    about: string,
+    posts: IPost[]
+    comments: IComment[]
+}
+
+const postSchema = new Schema<IPost>({
+    postId: {type: Number, required: true},
+    title: {type: String, required: true},
+    content: {type: String, required: true},
+}, {timestamps: true})
+
+const commentSchema = new Schema<IComment>({
+    commentId: {type: Number, required: true},
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    content: {type: String, required: true},
+}, {timestamps: true})
+
+const userSchema = new Schema<IUser>({
+    email: {type: String, required: true },
+    username: {type: String, required: true },
+    password: {type: String, required: true},
+    refreshToken: {type: String, default: ''},
+    gender: {type: String},
+    weight: {type: Number},
+    age: {type: Number},
+    style: {type: String},
+    followers: {type: Number, default: 0},
+    following: {type: Number, default: 0},
+    about: {type: String},
+    posts: [postSchema],
+    comments: [commentSchema]
+
+})
+
+const User = mongoose.models.users || mongoose.model("users", userSchema, "users")
+
+export default User;
