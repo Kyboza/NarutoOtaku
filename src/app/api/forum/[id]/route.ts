d4d5c5177bf304a680bd134e4d04c9e01b94a586
Johan Clifford <johanclifford746@gmail.com>
2025-03-27 22:54:19 +0100

    More fixes

--- src/app/api/forum/[id]/route.ts
+++ src/app/api/forum/[id]/route.ts
@@ -3,12 +3,11 @@ import SpecificForum from '@/app/models/SpecificForum'
 import { NextRequest, NextResponse } from 'next/server'
 import mongoose from 'mongoose'
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
+export async function GET(req: NextRequest) {
   try {
-    const { id } = await params
+    // Extract the 'id' from the URL path
+    const urlSegments = req.nextUrl.pathname.split('/');
+    const id = urlSegments[urlSegments.length - 1]; // Assuming the 'id' is the last segment
 
     if (!mongoose.Types.ObjectId.isValid(id)) {
       console.log(`${id} is not a valid ObjectId`)
@@ -18,28 +17,28 @@ export async function GET(
       )
     }
 
-    const CategoryId = new mongoose.Types.ObjectId(id)
+    const CategoryId = new mongoose.Types.ObjectId(id);
 
-    const connection = await connectToDatabase()
+    const connection = await connectToDatabase();
     if (!connection.success) {
-      return NextResponse.json({ message: connection.message }, { status: 500 })
+      return NextResponse.json({ message: connection.message }, { status: 500 });
     }
 
     const responseForumPosts = await SpecificForum.find({
       categoryId: CategoryId,
-    })
+    });
 
     if (responseForumPosts.length === 0) {
-      console.log(`No posts found for: ${CategoryId}`)
-      return NextResponse.json({ message: 'No posts found' }, { status: 400 })
+      console.log(`No posts found for: ${CategoryId}`);
+      return NextResponse.json({ message: 'No posts found' }, { status: 400 });
     }
 
-    return NextResponse.json(responseForumPosts, { status: 200 })
+    return NextResponse.json(responseForumPosts, { status: 200 });
   } catch (error) {
-    console.error('Error recieving the data:', error)
+    console.error('Error receiving the data:', error);
     return NextResponse.json(
-      { message: 'An error occured while getting the data' },
+      { message: 'An error occurred while getting the data' },
       { status: 500 }
-    )
+    );
   }
 }
